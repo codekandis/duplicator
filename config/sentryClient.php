397b29bde73cb08f52dec56cadb73d9b6011b7af
Christian Ramelow <info@codekandis.net>
2021-12-30 20:16:43 +0100

    Updates the release version `0.1.0`.

--- config/sentryClient.php
+++ config/sentryClient.php
@@ -1,4 +1,10 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\Duplicator\Configurations\Plain;
 
-return [];
+return [
+	'dsn'           => '',
+	'displayErrors' => false,
+	'environment'   => 'production',
+	'release'       => '0.1.0',
+	'serverName'    => 'duplicator.codekandis'
+];
